a2dd15c91fbdcc55b067e5bad1fa5cf68c0850bd
quangnm <quangnm16535@gmail.com>
2025-04-27 15:47:10 +0700

    Update add, remove, view

--- To-do list.py
+++ To-do list.py
@@ -1,5 +1,9 @@
-todo_list = []
-complete_tasks = []
+no = 0
+deadline = 0
+task = 0
+a = 0
+item = 0
+todo_list = [['No.', 'Deadline', 'Task']]
 
 
 def mark():
@@ -15,17 +19,32 @@ def complete():
 
 
 def view():
-    incomplete()
-    complete()
-    mark()
+    global item
+    for item in todo_list:
+        print("{:<5} {:<10} {:<20}".format(item[0], item[1], item[2]))
 
 
 def add():
-    pass
+    global no
+    no = len(todo_list)
+    global deadline
+    deadline = input('Input deadline: ')
+    global task
+    task = input('Input task: ')
+    global a
+    a = [no, deadline, task]
+    todo_list.append(a)
 
 
-def remv():
-    pass
+def remove():
+    global a
+    a = int(input('Remove task number: '))
+    todo_list.pop(a)
+    global item
+    for item in range(1, len(todo_list)):
+        b = todo_list[item]
+        b[0] = int(item)
+        todo_list[item] = b
 
 
 def save():
@@ -49,7 +68,7 @@ choice = int(input('Choose an option: '))
 while choice != 8:
 
     print('\n--- ADVANCE TO-DO APP ---\n1. View tasks\n2. Add task\n3. Remove task',
-          '\n4. Save task\n5. Load task\n6. Clear all task\n7. Search task',
+          '\n4. Save tasks\n5. Load tasks\n6. Clear all tasks\n7. Search task',
           '\n8. Exit')
 
     if choice == 1:
@@ -57,7 +76,7 @@ while choice != 8:
     elif choice == 2:
         add()
     elif choice == 3:
-        remv()
+        remove()
     elif choice == 4:
         save()
     elif choice == 5:
